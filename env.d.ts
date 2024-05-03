78e65bf0c743f1a504a4ec08a45b1f626db52571
jorgeguedess <jorge_guedes00@hotmail.com>
2024-05-03 16:29:38 -0300

    chore: added new environment variables

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,10 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    SERVER_PORT: number;
     DB_HOST: string;
     DB_PORT: number;
     DB_USERNAME: string;
     DB_PASSWORD: string;
+    DATABASE_URL: string;
   }
 }
